ab861ebf4df4a74e7e99f33360a826682f41f8d3
geon <101681047+teraterm@users.noreply.github.com>
2022-04-12 23:43:08 +0900

    Add files via upload

---
+++ pythonCode/CH3/sample.py
@@ -0,0 +1,9 @@
+#!/bin/env python
+#-*- coding: utf-8 -*-
+
+#코드 수정 참고 사이트 
+#https://zephyrus1111.tistory.com/39
+
+mymessages = open("mymessages.log", encoding= 'cp949')
+print (mymessages.read())
+mymessages.close()
